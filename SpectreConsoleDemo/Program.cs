d9c3b43e969d231e9b5278bfe4e9029c4000e982
Ryu <215513592@qq.com>
2022-03-25 18:14:42 +0800

    1.提交新的Demo工程

---
+++ SpectreConsoleDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace SpectreConsoleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new CanvasImage("1.jpg");
+            AnsiConsole.Write(c);
+            Console.ReadKey();
+        }
+
+    }
+}
